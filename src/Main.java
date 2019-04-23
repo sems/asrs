4ea08ba02d98d6bd3ad11f3940472d154bcb7a12
timonpost@hotmail.nl <timonpost@hotmail.nl>
2019-04-23 14:14:07 +0200

    project init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import View.StorageRackFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        StorageRackFrame asrPanel = new StorageRackFrame();
+        asrPanel.setVisible(true);
+
+    }
+}
