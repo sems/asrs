a645556a9450cdd27e40d486c44273aa39226259
timonpost@hotmail.nl <timonpost@hotmail.nl>
2019-04-23 14:17:10 +0200

    removed code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import View.StorageRackFrame;
-
 public class Main {
     public static void main(String[] args) {
-        StorageRackFrame asrPanel = new StorageRackFrame();
-        asrPanel.setVisible(true);
-
     }
 }
