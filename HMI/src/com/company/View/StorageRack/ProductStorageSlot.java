952204a43d0cdfa4e75777d5fe3c6342d319dabc
Timon <timonpost@hotmail.nl>
2019-04-24 14:12:00 +0200

    Added storage view component (#3)

---
+++ HMI/src/com/company/View/StorageRack/ProductStorageSlot.java
@@ -0,0 +1,16 @@
+package com.company.View.StorageRack;
+
+import java.awt.*;
+
+public class ProductStorageSlot extends StorageSlot {
+
+    public ProductStorageSlot(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void drawSlot(Graphics g) {
+        g.setColor(Color.black);
+        g.fillOval(this.getX(), this.getY(), 40, 40);
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# asrs
-This repo is the source of the code for the ASR, BINr and HMI
