22628b81220626c25d77532c4b0b311a692f41f4
GeorgiusNL <44576579+GeorgiusNL@users.noreply.github.com>
2019-05-13 14:34:26 +0200

    DifferentDirections
    the belt now can go different dirtections

--- Robots/BINr/app.ino
+++ Robots/BINr/app.ino
@@ -18,9 +18,13 @@ int BinrTime = 1000; //amount of miliseconds the conveyor belt
 int AsrTime = 1000;  //amount of miliseconds the ASR
                      //takes to replace a product
 
+int laserPin = 2;   //Arduino pin for laser detection
+boolean check = false;  //
+
 int powPin = 5;     //Arduino pin for power for motor
 int dirPin = 4;     //Arduino pin for motor direction
 
+int i = 0;          //counter integer
 int amount = 6;     //amount of items BINr need to replace
 
 
@@ -29,24 +33,40 @@ void setup() {
   
   pinMode(powPin, OUTPUT);  // motor power
   pinMode(dirPin, OUTPUT);  // motor direction
+
+  pinMode(laserPin, INPUT); // laser detection
+
+  Serial.begin(9600); // Setup serial connection for print out to console
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  for(int a = 0; a < amount; a++){  // for each product coming in
-    dir = dirs[a];
-    
-    digitalWrite(powPin, HIGH);     // turn on motor
-    if(dir){                        // check direction of product is to the right
-      digitalWrite(dirPin, HIGH);   // put motor direction to the right
-    } else {
-      digitalWrite(dirPin, LOW);    // put motor direction to the left
-    }
-    
-    delay(BinrTime);                 // grant the system time to process
-    digitalWrite(powPin, LOW);      // turn off motor
-    delay(AsrTime);                  // grant time to put in new product
 
+  int laser = digitalRead(laserPin);
+
+  
+  dir = dirs[i];
+  
+  while(digitalRead(laserPin) == HIGH){ // wait untill product is on belt
+    delay(100);  
+  }
+  
+  digitalWrite(powPin, HIGH);     // turn on motor
+  if(dir){                        // check direction of product is to the right
+    digitalWrite(dirPin, HIGH);   // put motor direction to the right
+  } else {
+    digitalWrite(dirPin, LOW);    // put motor direction to the left
+  }
+  
+  while(digitalRead(laserPin) == LOW){ // grant the system time to replace product
+    delay(10);
+  }
+  
+  digitalWrite(powPin, LOW);      // turn off motor
+
+  if(i >= 6){
+    i = 0;
+  } else {
+    i++;
   }
 }
