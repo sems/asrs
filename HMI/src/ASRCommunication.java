8c50169df8a07c59202bd418981cdc6aef23b5ad
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2019-05-15 14:08:06 +0200

    Fixed comms

--- HMI/src/ASRCommunication.java
+++ HMI/src/ASRCommunication.java
@@ -3,9 +3,6 @@ import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
 
-import java.io.OutputStream;
-import java.util.ArrayList;
-
 /**
  * Wraps the communication to and from the ASR robot.
  */
@@ -16,7 +13,13 @@ public class ASRCommunication implements SerialPortDataListener {
         comPort = port;
         comPort.openPort();
         comPort.addDataListener(this);
-        start();
+
+        try{
+            Thread.sleep(5000);
+            start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -24,8 +27,10 @@ public class ASRCommunication implements SerialPortDataListener {
         port.setBaudRate(115200);
         ASRCommunication r = new ASRCommunication(port);
 
-        r.start();
+        r.pick();
         r.gotoPos((byte) 3, (byte) 2);
+        r.stop();
+        r.unload();
     }
 
     public void sendPacket(Packet packet) {
@@ -79,163 +84,167 @@ public class ASRCommunication implements SerialPortDataListener {
         if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
             return;
 
-        try {
-            Thread.sleep(3000);
-        } catch (Exception e) {
+        while(comPort.bytesAvailable() > 0){
+            System.out.println("--= NEW PACKET =--");
+            byte[] sizeBuffer = new byte[1];
+            comPort.readBytes(sizeBuffer, 1);
+            byte size = sizeBuffer[0];
 
-        }
-
-        byte[] sizeBuffer = new byte[1];
-        comPort.readBytes(sizeBuffer, 1);
-        byte size = sizeBuffer[0];
-
-        byte commandId = 0;
-        while (commandId == 0) {
-            byte[] commandBuf = new byte[1];
-            comPort.readBytes(commandBuf, 1);
-            commandId = commandBuf[0];
-        }
-
-        byte[] payload = new byte[size];
-
-        int timeout = 100;
-
-        while (comPort.bytesAvailable() < size + 1) {
-            try {
-                System.out.println("Not all received - " + comPort.bytesAvailable() + "/ " + (size + 1) + " bytes");
-                Thread.sleep(timeout);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            byte commandId = 0;
+            while (commandId == 0) {
+                byte[] commandBuf = new byte[1];
+                comPort.readBytes(commandBuf, 1);
+                commandId = commandBuf[0];
             }
-        }
 
-        System.out.println("All " + (size + 1) + " bytes received");
-        comPort.readBytes(payload, size);
+            System.out.println("Payload size: " + size);
+            System.out.println("Command Id: " + commandId);
 
-        System.out.println("Payload content:");
-        for (int i = 0; i < size; i++) {
-            System.out.println(i + 1 + ": " + payload[i]);
-        }
+            byte[] payload = new byte[size];
 
-        byte checkBuffer[] = new byte[1];
-        comPort.readBytes(checkBuffer, 1);
-        long packetChecksum = checkBuffer[0];
+            int timeout = 100;
 
-        byte packet[] = new byte[size + 2];
-
-        packet[0] = size;
-        packet[1] = commandId;
-
-        System.arraycopy(payload, 0, packet, 2, payload.length);
-
-        CRC8 receivedChecksum = new CRC8();
-        receivedChecksum.update(packet, 0, packet.length);
-        long calcChecksum = receivedChecksum.getValue();
-
-        System.out.println("Received Check: " + packetChecksum + ", Calculated Checksum: " + calcChecksum);
-
-        if (packetChecksum == calcChecksum) {
-            System.out.println("Packet is valid");
-
-            if (commandId == 101) {
-                System.out.println("Response to GetStatus (101)");
-                if (size == 1) {
-                    System.out.println("Status " + payload[0]);
-                    System.out.println("Response is correct");
-                    // TODO: Add application call
-                } else {
-                    System.err.println("Size differs from expected");
+            while (comPort.bytesAvailable() < size + 1) {
+                try {
+                    //System.out.println("Not all received - " + comPort.bytesAvailable() + "/ " + (size + 1) + " bytes");
+                    Thread.sleep(timeout);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
 
-            if (commandId == 102) {
-                System.out.println("Response to Stop (102)");
-                if (size == 1) {
-                    System.out.println("Response is correct");
-                    // TODO: Add application call
-                }
-            }
+            //System.out.println("All " + (size + 1) + " bytes received");
+            comPort.readBytes(payload, size);
 
-            if (commandId == 103) {
-                System.out.println("Response to Start (103)");
-                if (size == 1) {
-                    System.out.println("Response is correct");
-                    // TODO: add application call
-                } else {
-                    System.err.println("Size differs from expected");
-                }
-            }
+            //System.out.println("Payload content:");
+            //for (int i = 0; i < size; i++) {
+            //    System.out.println(i + 1 + ": " + payload[i]);
+            //}
 
-            if (commandId == 104) {
-                System.out.println("Message response (104)");
-                if (size == 1) {
-                    System.out.println("Response is correct");
+            byte checkBuffer[] = new byte[1];
+            comPort.readBytes(checkBuffer, 1);
+            long packetChecksum = checkBuffer[0];
 
-                    for (char c : payload) {
-                        System.out.print(c);
-                    }
+            byte packet[] = new byte[size + 2];
 
-                    System.out.print("\n");
-                    // TODO: add application call
-                } else {
-                    System.err.println("Size differs from expected");
-                }
-            }
+            packet[0] = size;
+            packet[1] = commandId;
 
-            // getPos response 110
-            if (commandId == 110) {
-                System.out.println("Response to getPos (110)");
-                if (size == 2) {
-                    System.out.println("Asr is at position x: " + payload[0] + ", y: " + payload[1]);
+            System.arraycopy(payload, 0, packet, 2, payload.length);
 
-                    // TODO: add application call
-                } else {
-                    System.err.println("Size differs from expected");
-                }
-            }
+            CRC8 receivedChecksum = new CRC8();
+            receivedChecksum.update(packet, 0, packet.length);
+            long calcChecksum = receivedChecksum.getValue();
 
-            // gotoPos reponse 111
-            if (commandId == 111) {
-                if (size == 1) {
-                    if (payload[0] == 0) {
-                        System.out.println("GotoPos success");
+            if (packetChecksum == calcChecksum) {
+                System.out.println("Packet is valid");
 
+                if (commandId == 101) {
+                    System.out.println("Response to GetStatus (101)");
+                    if (size == 1) {
+                        System.out.println("Status " + payload[0]);
+                        System.out.println("Response is correct");
                         // TODO: Add application call
                     } else {
-                        System.out.println("GotoPos went wrong");
+                        System.err.println("Size differs from expected");
                     }
-                } else {
-                    System.err.println("size differs from expected");
                 }
-            }
 
-            if (commandId == 113) {
-                if (size == 1) {
-                    if (payload[0] == 0) {
-                        System.out.println("Pick succes");
-
+                if (commandId == 102) {
+                    System.out.println("Response to Stop (102)");
+                    if (size == 1) {
+                        System.out.println("Response is correct");
                         // TODO: Add application call
-                    } else {
-                        System.out.println("Pick went wrong");
                     }
-                } else {
-                    System.err.println("size differs from expected");
                 }
+
+                if (commandId == 103) {
+                    System.out.println("Response to Start (103)");
+                    if (size == 1) {
+                        System.out.println("Response is correct");
+
+                        // TODO: add application call
+                    } else {
+                        System.err.println("Size differs from expected");
+                    }
+                }
+
+                if (commandId == 104) {
+                    System.out.println("Message response (104)");
+                    if (size > 0) {
+                        System.out.println("Response is correct");
+
+                        System.out.print("ASR: ");
+
+                        for (byte c : payload) {
+                            System.out.print((char)c);
+                        }
+                        System.out.print("\n");
+                        // TODO: add application call
+                    } else {
+                        System.err.println("Size differs from expected");
+                    }
+                }
+
+                // getPos response 110
+                if (commandId == 110) {
+                    System.out.println("Response to getPos (110)");
+                    if (size == 2) {
+                        System.out.println("Asr is at position x: " + payload[0] + ", y: " + payload[1]);
+
+                        // TODO: add application call
+                    } else {
+                        System.err.println("Size differs from expected");
+                    }
+                }
+
+                // gotoPos response 111
+                if (commandId == 111) {
+                    if (size == 1) {
+                        if (payload[0] == 0) {
+                            System.out.println("GotoPos success");
+
+                            // TODO: Add application call
+                        } else {
+                            System.out.println("GotoPos went wrong");
+                        }
+                    } else {
+                        System.err.println("size differs from expected");
+                    }
+                }
+
+                if (commandId == 113) {
+                    if (size == 1) {
+                        if (payload[0] == 0) {
+                            System.out.println("Pick success");
+
+                            // TODO: Add application call
+                        } else {
+                            System.out.println("Pick went wrong");
+                        }
+                    } else {
+                        System.err.println("size differs from expected");
+                    }
+                }
+
+                if (commandId == 114) {
+                    System.out.println("Response to unload (114)");
+                    if (size == 1) {
+                        System.out.println("Response is correct");
+
+                        // TODO: Add Application call
+                    } else {
+                        System.err.println("size differs from expected");
+                    }
+                }
+
+            } else {
+                System.err.println("Packet is invalid");
             }
 
-            if (commandId == 114) {
-                System.out.println("Response to unload (114)");
-                if (size == 1) {
-                    System.out.println("Response is correct");
+            System.out.println("--= PACKET END =--");
 
-                    // TODO: Add Application call
-                } else {
-                    System.err.println("size differs from expected");
-                }
-            }
-
-        } else {
-            System.err.println("Packet is invalid");
+            System.out.println("Bytes still available: " + comPort.bytesAvailable());
         }
     }
 }
