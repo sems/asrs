32221a30be52364065a0c0aa663217300585b707
timonpost@hotmail.nl <timonpost@hotmail.nl>
2019-05-16 13:33:56 +0200

    example

---
+++ HMI/src/Logic/BINR.java
@@ -0,0 +1,19 @@
+package Logic;
+
+import java.util.ArrayList;
+
+enum PackingBox {
+    Left,
+    Right
+}
+
+class PackingLocation {
+    private PackingBox box;
+    private int StorageItemId;
+
+}
+public class BINR {
+    public ArrayList<PackingLocation> getPackingLocations(ArrayList<StorageItem> pickedItems) {
+        // verdeel items over dozen
+    }
+}
