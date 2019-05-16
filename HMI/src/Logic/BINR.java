0dd45a5106fe601b798cc33614a70e50c2581c99
timonpost@hotmail.nl <timonpost@hotmail.nl>
2019-05-16 13:58:08 +0200

    edits

--- HMI/src/Logic/BINR.java
+++ HMI/src/Logic/BINR.java
@@ -10,10 +10,37 @@ enum PackingBox {
 class PackingLocation {
     private PackingBox box;
     private int StorageItemId;
-
 }
+
+class Box {
+    private ArrayList<StorageItem> packetItems;
+    private PackingBox boxType;
+    private boolean boxClosed = false;
+
+    public Box(PackingBox boxType) {
+       this.packetItems = new ArrayList<>();
+       this.boxType = boxType;
+    }
+
+    public void addStorageItem(StorageItem storageItem) {
+        packetItems = new ArrayList<>();
+    }
+
+    public void closeBox() {
+        this.boxClosed = true;
+    }
+}
+
 public class BINR {
-    public ArrayList<PackingLocation> getPackingLocations(ArrayList<StorageItem> pickedItems) {
+    private Box leftBox;
+    private Box rightBox;
+
+    public BINR() {
+        leftBox = new Box(PackingBox.Left);
+        rightBox = new Box(PackingBox.Right);
+    }
+
+    public void packItems(ArrayList<StorageItem> itemsToPack) {
         // verdeel items over dozen
     }
 }
