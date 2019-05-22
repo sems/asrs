eb018c483ab98cdf51db91a88b9300cc29f3ee10
JanFokke <janfokke@famtennapel.com>
2019-05-22 12:22:25 +0200

    Commandlogic (#58)
    * update
    
    * goto working and bug fixes
    
    * binr support added
    
    * vscode compatable and warnings fixed
    
    * removed Old binr files
    
    * patch
    
    * fix

--- Robots/ASR/commands.hpp
+++ Robots/ASR/commands.hpp
@@ -1,3 +1,5 @@
+// The file contains the command that are registered by the core.
+
 #ifndef COMMAND_STATUS_HPP
 #define COMMAND_STATUS_HPP
 
@@ -7,10 +9,17 @@ class Core;
 class Communication;
 class Packet;
 
+// The status code gives insight in what the ASR or BINr are doing
 void statusCommand(Core& core, Communication& communication, Packet& packet);
 
+// The stop command causes the ARS or BINr to stop with the current action.
+// A start command is required to start the ASR or BINr.
 void stopCommand(Core& core, Communication& communication, Packet& packet);
+
+// Only needed if the ARS or BINr are stopped
 void startCommand(Core& core, Communication& communication, Packet& packet);
+
+// Returns the position of the ASR picker. position of 255 means unknown.
 void getPositionCommand(Core& core, Communication& communication, Packet& packet);
 
 // Long running commands
